33d96ddcc0a17018d04123054c65202d36f2c9e7
Sean <mingyi_127@126.com>
2019-05-03 09:09:29 +0800

    fix test: test file should end with Test

---
+++ src/test/java/com/dotgoing/utils/BTest.kt
@@ -0,0 +1,13 @@
+package com.dotgoing.utils
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class BTest {
+
+    @Test
+    fun testB() {
+        val s = 4 + 9
+        assertEquals(s, 13, "oh no")
+    }
+}
---
+++ src/test/java/com/dotgoing/utils/UtilsApplicationTest.kt
@@ -0,0 +1,18 @@
+package com.dotgoing.utils
+
+import org.junit.Test
+
+class UtilsApplicationTest {
+
+    @Test
+    fun testTwo() {
+        assert(true, { "hello " })
+        assert(true, { "hello " })
+    }
+
+    @Test
+    fun testOne() {
+        assert(true, { "hello " })
+        assert(true, { "hello " })
+    }
+}
